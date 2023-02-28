a17f7498621c8279f9f232be6cc0f6a69980f1e2
Evan-Wesurvive <125921709+Evan-Wesurvive@users.noreply.github.com>
2023-02-28 22:52:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#input phase
+radius = float(input ("input radius "))
+#process phase 
+#3.174 used as pi approx
+area=(3.174)*radius*radius
+perimeter = 2*(3.174)*radius
+#output phase
+print ("Area of a circle with radius", radius ," is ",area)
+print ("Perimeter of circle with radius ", radius, " is", perimeter)
